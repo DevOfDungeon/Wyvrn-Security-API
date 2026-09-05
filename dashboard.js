fabb9706497ce30e798ae43ece77c85215cb9e1b
aaron-warner45 <mahekagarwalla.14@gmail.com>
2026-09-05 18:04:00 +0530

    Initial Wyvrn dashboard setup

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+document.getElementById("totalApis").innerText = "124";
+
+document.getElementById("alerts").innerText = "18";
+
+document.getElementById("activeThreats").innerText = "4";
+
+document.getElementById("blockedRequests").innerText = "37";
+
+document.getElementById("riskScore").innerText = "73";
+
+document.getElementById("riskLabel").innerText = "HIGH";
+
+document.getElementById("userRole").innerText = "ADMIN";
