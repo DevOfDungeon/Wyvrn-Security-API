515e4055ec90b92176f5aea2963968422333a8dc
DevOfDungeon <chesstilllife@gmail.com>
2026-09-05 05:53:14 -0400

    Attached middleware onto the main.py file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from sentinel.middleware import SentinelMiddleware
+
+
 app = FastAPI(title="Sentinel Security Gateway")
+
+app.add_middleware(SentinelMiddleware)
 
 
 @app.get("/")
