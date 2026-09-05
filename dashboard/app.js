bff592a03157983d106554a1795129415da7507f
Ojaswini Singh <ojaswinisingh7@gmail.com>
2026-09-06 03:53:01 +0530

    update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,1 +1,16 @@
+const API_BASE = "";
 
+let events = [];
+let stats = {};
+let detectionCounts = {};
+
+const DETECTIONS = [
+    "SQL_INJECTION",
+    "BOLA_IDOR",
+    "AUTH_ABUSE",
+    "RATE_ABUSE",
+    "SENSITIVE_DATA_EXPOSURE",
+    "SENSITIVE_DATA_PATTERN",
+    "EXCESSIVE_DATA_EXPOSURE",
+    "BEHAVIORAL_ANOMALY"
+];
